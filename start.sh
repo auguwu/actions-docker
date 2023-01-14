15fa8398e2953da4639981acf50d3537c29e5566
Noel <cutie@floofy.dev>
2023-01-13 20:35:14 -0700

    Fix the runner name

--- start.sh
+++ start.sh
@@ -11,12 +11,20 @@ if [ -z "${GITHUB_ACCESS_TOKEN:-}" ]; then
 fi
 
 HOSTNAME=$(cat /etc/hostname)
+RUNNER_NAME=""
+if [ -z "${GITHUB_RUNNER_NAME:-}" ]; then
+  echo "[actions-runner] Setting default runner name to actions-runner-$HOSTNAME"
+  RUNNER_NAME="actions-runner-$HOSTNAME"
+else
+  echo "[actions-runner] Runner name is now [$GITHUB_RUNNER_NAME]"
+  RUNNER_NAME=$GITHUB_RUNNER_NAME
+fi
+
 ORGANIZATION=${GITHUB_ORGANIZATION}
 ACCESS_TOKEN=${GITHUB_ACCESS_TOKEN}
-RUNNER_NAME=${GITHUB_RUNNER_NAME:-"actions-runner-$HOSTNAME"}
 REGISTRATION_TOKEN=$(curl -sX POST -H "Authorization: token ${ACCESS_TOKEN}" https://api.github.com/orgs/${ORGANIZATION}/actions/runners/registration-token | jq .token --raw-output)
 
-echo "[actions-runner] I have setup the runner's registration token with the $ORGANIZATION organization and with runner name $RUNNER_NAME"
+echo "[actions-runner:$ORGANIZATION@$RUNNER_NAME] Runner registration was a success, now setting up runner..."
 
 mkdir -p $HOME/_work
 cd /opt/github/actions-runner
@@ -26,14 +34,14 @@ cd /opt/github/actions-runner
   --url https://github.com/${ORGANIZATION} \
   --token ${REGISTRATION_TOKEN}            \
   --disableupdate                          \
-  --name actions-runner                    \
+  --name ${RUNNER_NAME}                    \
   --runnergroup ""                         \
   --work "$HOME/_work"
 
-echo "[actions-runner:$RUNNER_NAME@$ORGANIZATION] The runner should be configured now!"
+echo "[actions-runner:$ORGANIZATION@$RUNNER_NAME] The runner should be configured now!"
 
 function cleanup() {
-  echo "[actions-runner] Destroying runner..."
+  echo "[actions-runner:$ORGANIZATION@$RUNNER_NAME] Destroying runner..."
   ./config.sh remove --token ${REGISTRATION_TOKEN}
 }
 
